6ac3a423ad05981420ede7c89751e16a87166796
Manpreet Kaur <kaurman2305@gmail.com>
2020-05-28 13:25:24 +0530

    Add Database

--- app.js
+++ app.js
@@ -2,8 +2,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // const date = require(__dirname + "/date.js"); Not using date to recude the complexity
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27018/contacts", { useNewUrlParser: true,useUnifiedTopology: true });
+
+// const numberSchema = new mongoose.Schema({
+//   number : String
+//
+// });
+// const Number = mongoose.model("Number",numberSchema );
+//
+// const emailSchema = new mongoose.Schema({
+//   email : String
+// });
+//
+// const Email = mongoose.model("Email", emailSchema);
+
+const contactSchema = new mongoose.Schema({
+  name : String,
+  mobileNo : [String],
+  email: [String]
+})
+
+const Contact = mongoose.model("Contact", contactSchema);
 
 const app = express();
 
@@ -13,11 +34,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("add-contact");
+  res.render("home");
 })
 
 app.post("/add", function(req,res){
-  console.log(req.body    )
+  //console.log(req.body    );
+  const contactName = req.body.name;
+  const contactEmail = req.body.email;
+  const contactTel = req.body.tel;
+
+  const newContact = new Contact({
+    name: contactName,
+    mobileNo:[contactEmail],
+    email: [contactTel]
+  })
+  newContact.save();
 });
 
 app.listen(3000, function(){
